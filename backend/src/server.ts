f48d68fcaefd6efb5461871259eb7f0e4f8c4fa3
musty <wire0721@gmail.com>
2026-04-02 23:14:52 +0200

    fixato cors tra url vercel/render

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "https://meritum-lab-1-tjfc.vercel.app",
     credentials: true,
   }),
 );
